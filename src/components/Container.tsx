185997a187fe3efe63f3cc1e997bfb1b1feba279
Charles Harwood <charles@harwood.digital>
2024-01-10 16:34:29 +1000

    refactor(perf): move conditional variant-based classnames outside the component scope for Container and Heading

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -2,6 +2,15 @@ import clsx from 'clsx';
 
 import { PolymorphicComponentProp } from '@/utils/type-helpers';
 
+const Y_PADDING_CLASSNAME_MAP: {
+  [key in ContainerProps['yPadding']]: string;
+} = {
+  xs: 'py-4 md:py-5',
+  sm: 'py-8 md:py-10',
+  base: 'py-20 md:py-28',
+  lg: 'py-32 md:py-48',
+};
+
 type ContainerProps = {
   yPadding?: 'xs' | 'sm' | 'base' | 'lg';
   collapsible?: boolean;
@@ -21,14 +30,7 @@ export function Container<AsComponent extends React.ElementType = 'div'>({
   return (
     <Component
       data-collapsible={collapsible}
-      className={clsx(
-        'px-6',
-        yPadding === 'xs' && 'py-4 md:py-5',
-        yPadding === 'sm' && 'py-8 md:py-10',
-        yPadding === 'base' && 'py-20 md:py-28',
-        yPadding === 'lg' && 'py-32 md:py-48',
-        className,
-      )}
+      className={clsx('px-6', Y_PADDING_CLASSNAME_MAP[yPadding], className)}
       {...rest}>
       <div className="mx-auto w-full max-w-6xl">{children}</div>
     </Component>
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -2,6 +2,15 @@ import clsx from 'clsx';
 
 import { PolymorphicComponentProp } from '@/utils/type-helpers';
 
+const SIZE_CLASSNAME_MAP: {
+  [key in HeadingProps['size']]: string;
+} = {
+  xl: 'text-6xl md:text-8xl',
+  lg: 'text-5xl md:text-6xl',
+  md: 'text-4xl md:text-5xl',
+  sm: 'text-3xl md:text-4xl',
+};
+
 type HeadingProps = {
   size?: 'xl' | 'lg' | 'md' | 'sm';
   className?: string;
@@ -21,10 +30,7 @@ export function Heading<AsComponent extends React.ElementType = 'h1'>({
     <Component
       className={clsx(
         'heading',
-        size === 'xl' && 'text-6xl md:text-8xl',
-        size === 'lg' && 'text-5xl md:text-6xl',
-        size === 'md' && 'text-4xl md:text-5xl',
-        size === 'sm' && 'text-3xl md:text-4xl',
+        SIZE_CLASSNAME_MAP[size],
         hasHoverShadowTransition && 'heading--shadow-transition',
         className,
       )}>
