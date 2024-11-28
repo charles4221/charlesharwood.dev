80d450d221dfb84e43bfe8e454241f757e335d64
Charles Harwood <charles@harwood.digital>
2024-11-28 12:37:39 +1000

    fix(metadata): use ACTUALLY correct vercel env var for baseurl

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,7 +4,8 @@ export const IS_DEV = process.env.NODE_ENV !== 'production';
 export const IS_TEST = process.env.NODE_ENV === 'test';
 
 export const BASE_URL =
-  process.env.VERCEL_URL || `http://localhost:${process.env.PORT || 3000}`;
+  process.env.VERCEL_PROJECT_PRODUCTION_URL ||
+  `http://localhost:${process.env.PORT || 3000}`;
 
 export const METADATA_BASE: Metadata['metadataBase'] = new URL(BASE_URL);
 
